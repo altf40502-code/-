6d75305ce70994cfd8d65547ddc41f9b2e6d8a69
altf40502-code <altf40502@gmail.com>
2026-04-08 18:54:40 +0900

    Add files via upload

--- 21124허정욱_수행평가1연습.c
+++ 21124허정욱_수행평가1연습.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    
-    
+    int dan = 5; 
     int i;
-    int sum = 0; 
-    for (i = 1; i <= 10; i++) { // 10 포함해야함 
-        sum = sum + i;
-    }
 
-    printf("1부터 10까지의 합: %d\n", sum);
-    if (sum % 2 == 0) { 
-        printf("결과는 짝수입니다.\n"); // "로 묶어야 함 
-    } else {
-        printf("결과는 홀수입니다.\n"); // 세미콜론 빠짐 
+    for (i = 1; i <= 9; i++) { //i를 1부터 시작 9까지 반복 
+        printf("%d * %d = %d\n", dan, i, dan * i); //세미콜론 빠짐 
     }
 
     return 0;
